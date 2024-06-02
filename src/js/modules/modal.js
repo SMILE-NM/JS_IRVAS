ed96925efc0dc618f3c07ba656f53d69e42b249d
SMILE-NM <123355875+SMILE-NM@users.noreply.github.com>
2024-06-02 23:26:18 +0500

    Form state and changeModalState created

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,26 +1,44 @@
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  function bindModal(
+    triggerSelector,
+    modalSelector,
+    closeSelector,
+    closeClickOverlay = true,
+  ) {
     const triggers = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector);
-
+      close = document.querySelector(closeSelector),
+      window = document.querySelectorAll('[data-modal]');
     triggers.forEach((item) => {
       item.addEventListener('click', (e) => {
         if (e.target) {
           e.preventDefault();
         }
+
+        window.forEach((item) => {
+          item.style.display = 'none';
+        });
+
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
       });
     });
 
     close.addEventListener('click', () => {
+      window.forEach((item) => {
+        item.style.display = 'none';
+      });
+
       modal.style.display = 'none';
       document.body.style.overflow = '';
     });
 
     modal.addEventListener('click', (e) => {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        window.forEach((item) => {
+          item.style.display = 'none';
+        });
+
         modal.style.display = 'none';
         document.body.style.overflow = '';
       }
@@ -40,6 +58,19 @@ const modals = () => {
     '.popup_engineer .popup_close',
   );
   bindModal('.phone_link', '.popup', '.popup .popup_close');
+  bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+  bindModal(
+    '.popup_calc_button',
+    '.popup_calc_profile',
+    '.popup_calc_profile_close',
+    false,
+  );
+  bindModal(
+    '.popup_calc_profile_button',
+    '.popup_calc_end',
+    '.popup_calc_end_close',
+    false,
+  );
 
   // showModalByTime('.popup', 60000);
 };
